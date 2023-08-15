2e7a1e012a005c128c7bf86c54d3b38b5812de2c
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-08-15 15:39:05 -0300

    landing style media

--- src/Views/Landing/LandingStyle.js
+++ src/Views/Landing/LandingStyle.js
@@ -3,9 +3,7 @@ import { motion } from "framer-motion";
 
 
 export const LogInDiv = styled(motion.div)`
-position: absolute;
-right: 50%;
-top: 40%;
+width: fit-content;
 color: #354A21;
 background-color: white;
 border: 1px solid white;
@@ -17,15 +15,22 @@ transition: 1s;
 font-weight: 500;
 font-size: 20px;
 
+position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 :hover{
 background-color: white;
 color: #354A21;
 }
+
+
+
 `;
 
 
 export const MainLandingDiv = styled(motion.div)`
-background-color: #333;
+color: rgba(188,188,188);
 width: 100vw;
 height: 100vh;
 overflow: hidden;
@@ -34,44 +39,76 @@ overflow: hidden;
     top: 10%;
     right: -10%;
     width: 90%;
-
+    @media (max-width: 430px) {
+      width: 150%;
+    }
   }
     .photo2{
     position: relative;
     bottom: -25%;
     left: 100%;
     width: 90%;
+    @media (max-width: 430px) {
+      width: 150%;
+      bottom: -45%;
+      left: 100%;
+    }
   }
   .photo3 {
     position: relative;
     bottom: -50%;
     left: 0%;
     width: 60%;
+    @media (max-width: 430px) {
+      width: 100%;
+      bottom: -15%;
+      left: 0%;
+    }
   }
   .photo5 {
     position: relative;
     top: -60%;
     left: 150%;
     width: 80%;
+    @media (max-width: 430px) {
+      top: 35%;
+      left: 0%;
+      width: 150%;
+    }
   }
   .photo6 {
     position: relative;
     top: 30%;
     left: 40%;
     width: 100%;
+    @media (max-width: 430px) {
+      top: 30%;
+    left: 40%;
+    width: 200%;
+    }
   }
   .photo7 {
     position: relative;
     top:20%;
     left: -140%;
     width: 66%;
+    @media (max-width: 430px) {
+    top:20%;
+    left: -140%;
+    width: 100%;
+    }
   }
   .photo8 {
     position: relative;
     bottom: -10%;
     right: 140%;
     width: 60%;
+    @media (max-width: 430px) {
     border: 1px solid blue;
+    bottom: -30%;
+    left: 75%;
+    width: 170%;
+    }
   }
 `;
 
